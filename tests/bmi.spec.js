a7715285d1182430f55f8391ccdf22eb8eeab9e7
Maor <mymaor89@gmail.com>
2020-01-12 15:28:19 +0200

    testing inital setup

---
+++ tests/bmi.spec.js
@@ -0,0 +1,8 @@
+var chai = require('chai')
+var expect = require('chai').expect;
+describe('test suite name', () => {
+    it('should all be fine', () => {})
+    it('fail for testing', () => {
+        expect(4).to.be.equal(4)
+    })
+})
