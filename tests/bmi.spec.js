959f5bfa1b02bf8715be1e9e8193651ef08b1a76
Maor <mymaor89@gmail.com>
2020-01-14 11:27:44 +0200

    bmi tests

--- tests/bmi.spec.js
+++ tests/bmi.spec.js
@@ -1,8 +1,26 @@
 var chai = require('chai')
 var expect = require('chai').expect;
-describe('test suite name', () => {
-    it('should all be fine', () => {})
-    it('fail for testing', () => {
-        expect(4).to.be.equal(4)
+var bmi = require('../bmi').bmi
+describe('BMI', () => {
+    it('should output healthy', () => {
+        expect(bmi(170,60)).to.be.equal('healthy')
     })
+    it('should output healthy', () => {
+        expect(bmi(170,53)).to.be.equal('underweight')
+    })
+    it('should output healthy', () => {
+        expect(bmi(170,73)).to.be.equal('overweight')
+    })
+    it('should output healthy', () => {
+        expect(bmi(170,87)).to.be.equal('obese')
+    })
+    describe('Invalid params',()=>{
+        it('should return -1 for invalid values', () => {
+            expect(bmi(170,0)).to.be.equal(-1)
+        })
+        it('should return -1 for invalid values', () => {
+            expect(bmi(0,60)).to.be.equal(-1)
+        })
+    })
+    
 })
