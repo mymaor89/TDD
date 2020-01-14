be1dbdcbe57426ee7963ab3ce2aa2cc61ac0b256
Gosha <djabarov.george@gmail.com>
2020-01-14 14:00:45 +0200

    added bmi calculations

--- bmi.js
+++ bmi.js
@@ -1,7 +1,45 @@
 
-function bmi(height_cm, weight_kg)
+module.exports = function bmi(height_cm, weight_kg)
 {
-
+   height = height_cm/100;
+   bmi_result = weight_kg/(Math.pow(height,2))
+   switch(true) {
+      case bmi_result <= 0:
+         {
+            result = -1
+            break;
+         }
+      case bmi_result < 18.5:
+         {
+            result = "underweight"
+            break;
+         }
+      case  bmi_result < 25:
+         {
+            result = "healthy"
+            break;
+         }
+      case bmi_result < 30:
+         {
+            result = "overweight"
+            break;
+         }
+      case bmi_result < 40:
+         {
+            result = "obese"
+            break;
+         }
+      case 40 > bmi_result:
+         {
+            result = -1
+            break;
+         }
+      default:
+         {
+            result = -1
+         }
+      }
+   return result;
 }
 
-module.exports = bmi;
+
--- tests/bmi.spec.js
+++ tests/bmi.spec.js
@@ -1,18 +1,20 @@
 var chai = require('chai')
 var expect = require('chai').expect;
-var bmi = require('../bmi').bmi
+var bmi = require('../bmi');
 describe('BMI', () => {
-    it('should output healthy', () => {
-        expect(bmi(170,60)).to.be.equal('healthy')
-    })
-    it('should output healthy', () => {
-        expect(bmi(170,53)).to.be.equal('underweight')
-    })
-    it('should output healthy', () => {
-        expect(bmi(170,73)).to.be.equal('overweight')
-    })
-    it('should output healthy', () => {
-        expect(bmi(170,87)).to.be.equal('obese')
+    describe('Valid params',()=>{
+        it('should output healthy', () => {
+            expect(bmi(170,60)).to.be.equal('healthy')
+        })
+        it('should output underweight', () => {
+            expect(bmi(170,53)).to.be.equal('underweight')
+        })
+        it('should output overweight', () => {
+            expect(bmi(170,78)).to.be.equal('overweight')
+        })
+        it('should output obese', () => {
+            expect(bmi(170,97)).to.be.equal('obese')
+        })
     })
     describe('Invalid params',()=>{
         it('should return -1 for invalid values', () => {
