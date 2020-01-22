1e7a2174f7095f19c5eb29412122aa2963d04050
Maor <mymaor89@gmail.com>
2020-01-22 10:53:22 +0200

    bmi now using bmi_calc

--- bmi.js
+++ bmi.js
@@ -1,42 +1,42 @@
 
+const bmi_calc = require('./bmi.calc')
 module.exports = function bmi(height_cm, weight_kg)
 {
-   height = height_cm/100;
-   bmi_result = weight_kg/(Math.pow(height,2))
+   bmi_result = bmi_calc(height_cm,weight_kg);
    switch(true) {
       case bmi_result <= 0:
          {
-            result = -1
+            result = -1;
             break;
          }
       case bmi_result < 18.5:
          {
-            result = "underweight"
+            result = "underweight";
             break;
          }
       case  bmi_result < 25:
          {
-            result = "healthy"
+            result = "healthy";
             break;
          }
       case bmi_result < 30:
          {
-            result = "overweight"
+            result = "overweight";
             break;
          }
       case bmi_result < 40:
          {
-            result = "obese"
+            result = "obese";
             break;
          }
       case 40 > bmi_result:
          {
-            result = -1
+            result = -1;
             break;
          }
       default:
          {
-            result = -1
+            result = -1;
          }
       }
    return result;
