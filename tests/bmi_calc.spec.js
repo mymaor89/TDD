1a7dd4c9c1853988863f4e9e742eb6f0c746b08e
Maor <mymaor89@gmail.com>
2020-01-22 12:26:57 +0200

    fixed bmi_calc titles

--- tests/bmi_calc.spec.js
+++ tests/bmi_calc.spec.js
@@ -6,13 +6,13 @@ describe('BMI-Calc', () => {
         it('should return correct value', () => {
             expect(bmi_calc(170,60)).to.be.closeTo(20.76124567, 0.001)
         })
-        it('should output underweight', () => {
+        it('should return correct value', () => {
             expect(bmi_calc(170,53)).to.be.closeTo(18.33910035, 0.001)
         })
-        it('should output overweight', () => {
+        it('should return correct value', () => {
             expect(bmi_calc(170,78)).to.be.closeTo(26.98961938, 0.001)
         })
-        it('should output obese', () => {
+        it('should return correct value', () => {
             expect(bmi_calc(170,97)).to.be.closeTo(33.56401384, 0.001)
         })
     })
